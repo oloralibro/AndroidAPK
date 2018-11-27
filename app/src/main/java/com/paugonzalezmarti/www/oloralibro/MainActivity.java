46f1b46d2df780a1ac9331d1c41058027161a805
oloralibro <freespamgratis@gmail.com>
2018-11-27 13:34:16 +0100

    El botó per registrar-se del login t'envia al layout de registre

--- app/src/main/java/com/paugonzalezmarti/www/oloralibro/MainActivity.java
+++ app/src/main/java/com/paugonzalezmarti/www/oloralibro/MainActivity.java
@@ -2,6 +2,9 @@ package com.paugonzalezmarti.www.oloralibro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        TextView registro = findViewById(R.id.tvRegistro);
+        EditText usuario = findViewById(R.id.etCorreo);
+        EditText contrasenya = findViewById(R.id.etPassword);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.registro);
+            }
+        });
     }
+
 }
